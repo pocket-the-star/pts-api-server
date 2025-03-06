eeb3a19d4f4a9bd8760c1aacbff24a43d669d067
SeongJongHo <whdgh9595@gmail.com>
2025-03-06 14:40:26 +0900

    Feat(idol): artist groupId 필드 추가

--- src/main/java/com/pts/api/idol/domain/model/Artist.java
+++ src/main/java/com/pts/api/idol/domain/model/Artist.java
@@ -4,12 +4,15 @@ import com.pts.api.lib.internal.shared.base.model.BaseModel;
 import java.time.LocalDateTime;
 
 public class Artist extends BaseModel {
+
     private Long id;
+    private Long groupId;
     private String name;
 
-    public Artist(Long id, String name,
-                  LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+    public Artist(Long id, Long groupId, String name,
+        LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
         this.id = id;
+        this.groupId = groupId;
         this.name = name;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
