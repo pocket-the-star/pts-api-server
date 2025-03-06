2990e25450143fcb546f4f8468a33d282e76ee68
SeongJongHo <whdgh9595@gmail.com>
2025-03-06 23:13:45 +0900

    Feat(mail): auth mail adapter 작성

---
+++ src/main/java/com/pts/api/mail/infrastructure/adapter/AuthMailSenderAdapterImpl.java
@@ -0,0 +1,30 @@
+package com.pts.api.mail.infrastructure.adapter;
+
+import com.pts.api.mail.application.port.out.AuthMailSenderPort;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthMailSenderAdapterImpl implements AuthMailSenderPort {
+
+    private final JavaMailSender javaMailSender;
+    private static final String SUBJECT = "포켓더스타 인증 코드 안내";
+    private static final String TEXT = "인증 코드: %s";
+
+    @Async
+    @Override
+    public void send(String email, String authCode) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(SUBJECT);
+        message.setText(String.format(TEXT, authCode));
+
+        javaMailSender.send(message);
+    }
+}
