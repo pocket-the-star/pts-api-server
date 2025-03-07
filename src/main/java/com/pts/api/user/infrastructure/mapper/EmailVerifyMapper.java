bff6ff39a7028172e165afaea1f904de346a7215
SeongJongHo <whdgh9595@gmail.com>
2025-03-07 22:42:10 +0900

    Feat(user): Mapper 구현체 작성

---
+++ src/main/java/com/pts/api/user/infrastructure/mapper/EmailVerifyMapper.java
@@ -0,0 +1,32 @@
+package com.pts.api.user.infrastructure.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailVerifyMapper<T> implements IEmailVerifyMapper<T> {
+
+    private final ObjectMapper objectMapper;
+
+
+    @Override
+    public String mapToString(T object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public T mapToObject(String value, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(value, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
