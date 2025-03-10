e3273fd2f252128488f1b3a7383fd2f2f2f08e8c
SeongJongHo <whdgh9595@gmail.com>
2025-03-10 21:32:23 +0900

    Feat(User): UserRole String 인자 추가

--- src/main/java/com/pts/api/lib/internal/shared/enums/UserRole.java
+++ src/main/java/com/pts/api/lib/internal/shared/enums/UserRole.java
@@ -1,7 +1,26 @@
 package com.pts.api.lib.internal.shared.enums;
 
 public enum UserRole {
-    ADMIN,
-    SELLER,
-    NORMAL
+    ADMIN("ADMIN"),
+    SELLER("SELLER"),
+    NORMAL("NORMAL");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public static UserRole of(String role) {
+        for (UserRole userRole : values()) {
+            if (userRole.getRole().equals(role)) {
+                return userRole;
+            }
+        }
+        throw new IllegalArgumentException("Invalid role: " + role);
+    }
 }
