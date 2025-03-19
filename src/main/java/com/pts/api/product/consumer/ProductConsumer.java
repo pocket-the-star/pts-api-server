f9cecbb5ba5b9af8d1ddb53bb9a97afb864514e7
SeongJongHo <whdgh9595@gmail.com>
2025-03-19 16:22:50 +0900

    Feat(Product): 상품 업데이트 컨슈머 작성

---
+++ src/main/java/com/pts/api/product/consumer/ProductConsumer.java
@@ -0,0 +1,49 @@
+package com.pts.api.product.consumer;
+
+import com.pts.api.global.common.exception.NotFoundException;
+import com.pts.api.lib.internal.shared.event.Event;
+import com.pts.api.lib.internal.shared.event.EventData;
+import com.pts.api.lib.internal.shared.event.EventType.Topic;
+import com.pts.api.lib.internal.shared.event.data.FeedCreateData;
+import com.pts.api.product.service.ProductService;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ProductConsumer {
+
+    private final ProductService productService;
+
+    @KafkaListener(topics = {
+        Topic.FEED_CREATE
+    }, groupId = "product-group")
+    public void feedCreate(String message) {
+        try {
+            Event<EventData> event = Event.fromJson(message);
+            handleEvent(Objects.requireNonNull(event));
+
+            log.info("상품 메세지 처리 완료: {}", message);
+        } catch (Exception e) {
+
+            log.error("상품 메시지 처리 중 오류 발생. 메시지: {}", message, e);
+        }
+    }
+
+    private void handleEvent(Event<EventData> event) {
+        switch (event.getType()) {
+            case FEED_CREATE:
+                FeedCreateData feedCreateData = (FeedCreateData) event.getData();
+                productService.priceUpdate(feedCreateData);
+                break;
+            default:
+                throw new NotFoundException("존재하지 않는 이벤트 타입입니다.: " + event.getType());
+        }
+    }
+
+
+}
