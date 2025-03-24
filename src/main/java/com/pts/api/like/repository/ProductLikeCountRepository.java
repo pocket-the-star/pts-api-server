9afa290104c4a5145a9e2a658b40eae368205f4a
SeongJongHo <whdgh9595@gmail.com>
2025-03-24 12:32:33 +0900

    Feat(like): product like count 리포지터리 작성

--- src/main/java/com/pts/api/like/repository/ProductLikeCountRepository.java
+++ src/main/java/com/pts/api/like/repository/ProductLikeCountRepository.java
@@ -1,5 +1,33 @@
 package com.pts.api.like.repository;
 
-public interface ProductLikeCountRepository {
+import com.pts.api.like.model.ProductLikeCount;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductLikeCountRepository extends JpaRepository<ProductLikeCount, Long> {
+
+    @Query(
+        value = """
+            update product_like_count
+            set count = count + 1
+            where product_id = :productId
+            """,
+        nativeQuery = true
+    )
+    @Modifying
+    void increase(@Param("productId") Long productId);
+
+    @Query(
+        value = """
+            update product_like_count
+            set count = count - 1
+            where product_id = :productId
+            """,
+        nativeQuery = true
+    )
+    @Modifying
+    void decrease(@Param("productId") Long productId);
 
 }
