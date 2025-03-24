ffd691e9e9f9b479577100b244399d0b110211f1
SeongJongHo <whdgh9595@gmail.com>
2025-03-24 12:14:24 +0900

    Feat(like): product like 컨트롤러 작성

---
+++ src/main/java/com/pts/api/like/controller/ProductLikeController.java
@@ -0,0 +1,50 @@
+package com.pts.api.like.controller;
+
+
+import com.pts.api.global.presentation.response.BaseResponse;
+import com.pts.api.global.presentation.response.ResponseGenerator;
+import com.pts.api.global.presentation.response.ResponseMsg;
+import com.pts.api.like.service.ProductLikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "좋아요", description = "상품 좋아요 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/products/{productId}/likes")
+public class ProductLikeController {
+
+    private final ProductLikeService productLikeService;
+
+    @Operation(summary = "좋아요", description = "상품을 좋아요합니다.")
+    @PostMapping
+    public ResponseEntity<BaseResponse<Void>> post(
+        @AuthenticationPrincipal(expression = "principal") Long userId,
+        @PathVariable Long productId) {
+
+        productLikeService.like(productId, userId);
+
+        return ResponseGenerator.ok(ResponseMsg.OK, HttpStatus.OK);
+    }
+
+    @Operation(summary = "좋아요 취소", description = "상품 좋아요를 취소합니다.")
+    @DeleteMapping
+    public ResponseEntity<BaseResponse<Void>> delete(
+        @AuthenticationPrincipal(expression = "principal") Long userId,
+        @PathVariable Long productId) {
+
+        productLikeService.unlike(productId, userId);
+
+        return ResponseGenerator.ok(ResponseMsg.OK, HttpStatus.OK);
+    }
+
+}
