172433d64c3671a598b52be00c031015910bd8f7
SeongJongHo <whdgh9595@gmail.com>
2025-03-25 16:15:50 +0900

    Feat(global): AuthFilter 에러 핸들링 리스폰스 로직 추가

--- src/main/java/com/pts/api/global/presentation/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/pts/api/global/presentation/security/filter/JwtAuthenticationFilter.java
@@ -1,8 +1,9 @@
 package com.pts.api.global.presentation.security.filter;
 
 import com.pts.api.global.common.exception.UnauthorizedException;
+import com.pts.api.global.presentation.response.ResponseGenerator;
 import com.pts.api.lib.external.security.model.CustomAuthenticationToken;
-import com.pts.api.user.exception.InvalidTokenException;
+import com.pts.api.lib.internal.shared.util.serializer.DataSerializer;
 import com.pts.api.user.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,12 +11,14 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -33,6 +36,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         @NonNull FilterChain filterChain)
         throws ServletException, IOException {
         String bearerToken = request.getHeader("Authorization");
+        log.info("request endpoint: {}", request.getRequestURI());
         if (bearerToken != null) {
             try {
                 String refreshToken = extractRefreshToken(request.getCookies());
@@ -43,9 +47,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 CustomAuthenticationToken authenticationToken =
                     new CustomAuthenticationToken(tokenService.getUserId(bearerToken), authorities);
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            } catch (InvalidTokenException e) {
-                log.error("Invalid Token", e);
-                throw new UnauthorizedException("Invalid Token");
+            } catch (Exception e) {
+                log.error("Error Endpoint: {}, Msg: {}", request.getRequestURI(), e.toString());
+                handleErrorResponse(response, e.getMessage());
             }
         }
         filterChain.doFilter(request, response);
@@ -53,7 +57,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private String extractRefreshToken(Cookie[] cookies) {
         if (cookies == null) {
-            return null;
+            throw new UnauthorizedException("리프레시 토큰은 필수입니다.");
         }
         return Arrays.stream(cookies)
             .filter(cookie -> "refreshToken".equals(cookie.getName()))
@@ -66,5 +70,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
 
         return Collections.singletonList(authority);
+    }
+
+    private void handleErrorResponse(HttpServletResponse response, String errorMessage)
+        throws IOException {
+        String result = DataSerializer.serialize(
+            ResponseGenerator.fail(errorMessage, HttpStatus.UNAUTHORIZED).getBody());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json; charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
     }
 }
