9f079e3569274bdb880077f6c8831f24395e4ff1
SeongJongHo <whdgh9595@gmail.com>
2025-03-26 17:18:34 +0900

    Fix(order): 변경된 FeedRepositoryPort로 변경

--- src/main/java/com/pts/api/order/service/OrderFacade.java
+++ src/main/java/com/pts/api/order/service/OrderFacade.java
@@ -1,7 +1,7 @@
 package com.pts.api.order.service;
 
-import com.pts.api.feed.infrastructure.persistence.repository.FeedRepository;
-import com.pts.api.feed.model.Feed;
+import com.pts.api.feed.application.port.out.FeedRepositoryPort;
+import com.pts.api.feed.domain.model.Feed;
 import com.pts.api.global.common.exception.NotFoundException;
 import com.pts.api.order.dto.request.CreateOrderRequestDto;
 import com.pts.api.order.model.Order;
@@ -15,11 +15,11 @@ public class OrderFacade {
 
     private final OrderService orderService;
     private final ShippingService shippingService;
-    private final FeedRepository feedRepository;
+    private final FeedRepositoryPort feedRepository;
 
     @Transactional
     public void create(Long userId, CreateOrderRequestDto createOrderRequestDto) {
-        Feed feed = feedRepository.findById(createOrderRequestDto.feedId())
+        Feed feed = feedRepository.findOneById(createOrderRequestDto.feedId())
             .orElseThrow(() -> new NotFoundException(
                 "존재하지 않는 피드입니다. feedId=" + createOrderRequestDto.feedId()));
 
