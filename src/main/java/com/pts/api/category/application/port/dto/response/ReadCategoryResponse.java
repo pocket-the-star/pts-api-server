cde6289d134f417291cab26e68ddaabcd3db98ef
SeongJongHo <whdgh9595@gmail.com>
2025-04-09 16:05:43 +0900

    Feat(category): 카테고리 변환 로직 dto로 이관

--- src/main/java/com/pts/api/category/application/port/dto/response/ReadCategoryResponse.java
+++ src/main/java/com/pts/api/category/application/port/dto/response/ReadCategoryResponse.java
@@ -1,8 +1,17 @@
 package com.pts.api.category.application.port.dto.response;
 
+import com.pts.api.category.domain.model.Category;
 import java.time.LocalDateTime;
 
 public record ReadCategoryResponse(Long id, String name, LocalDateTime createdAt,
                                    LocalDateTime updatedAt) {
 
+    public static ReadCategoryResponse fromModel(Category category) {
+        return new ReadCategoryResponse(
+            category.getId(),
+            category.getName(),
+            category.getCreatedAt(),
+            category.getUpdatedAt()
+        );
+    }
 }
--- src/main/java/com/pts/api/category/application/service/CategoryService.java
+++ src/main/java/com/pts/api/category/application/service/CategoryService.java
@@ -4,7 +4,6 @@ import com.pts.api.category.application.port.dto.response.ReadCategoryResponse;
 import com.pts.api.category.application.port.in.ReadCategoryUseCase;
 import com.pts.api.category.application.port.out.CategoryRepositoryPort;
 import com.pts.api.category.common.exception.CategoryNotFoundException;
-import com.pts.api.category.domain.model.Category;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,23 +18,16 @@ public class CategoryService implements ReadCategoryUseCase {
     @Override
     @Transactional(readOnly = true)
     public ReadCategoryResponse getCategory(Long id) {
-        return mapToDto(categoryRepositoryPort.findOneById(id)
+        return ReadCategoryResponse.fromModel(categoryRepositoryPort.findOneById(id)
             .orElseThrow(() -> new CategoryNotFoundException("Category not found with id: " + id)));
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<ReadCategoryResponse> getCategories() {
-        return categoryRepositoryPort.findAll().stream().map(this::mapToDto).toList();
+        return categoryRepositoryPort.findAll().stream().map(ReadCategoryResponse::fromModel)
+            .toList();
     }
 
-    private ReadCategoryResponse mapToDto(Category category) {
-        return new ReadCategoryResponse(
-            category.getId(),
-            category.getName(),
-            category.getCreatedAt(),
-            category.getUpdatedAt()
-        );
-    }
 
 }
