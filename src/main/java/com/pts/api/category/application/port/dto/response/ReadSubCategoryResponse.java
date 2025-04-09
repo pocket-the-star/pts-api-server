637d59cbc43a923b5c4797602ad6fa9701d5221c
SeongJongHo <whdgh9595@gmail.com>
2025-04-09 16:06:49 +0900

    Feat(category): 서브 카테고리 변환 로직 dto로 이관

--- src/main/java/com/pts/api/category/application/port/dto/response/ReadSubCategoryResponse.java
+++ src/main/java/com/pts/api/category/application/port/dto/response/ReadSubCategoryResponse.java
@@ -1,9 +1,19 @@
 package com.pts.api.category.application.port.dto.response;
 
+import com.pts.api.category.domain.model.SubCategory;
 import java.time.LocalDateTime;
 
 public record ReadSubCategoryResponse(Long id, String name, Long categoryId,
                                       LocalDateTime createdAt,
                                       LocalDateTime updatedAt) {
 
+    public static ReadSubCategoryResponse fromModel(SubCategory subCategory) {
+        return new ReadSubCategoryResponse(
+            subCategory.getId(),
+            subCategory.getName(),
+            subCategory.getCategoryId(),
+            subCategory.getCreatedAt(),
+            subCategory.getUpdatedAt()
+        );
+    }
 }
--- src/main/java/com/pts/api/category/application/service/SubCategoryService.java
+++ src/main/java/com/pts/api/category/application/service/SubCategoryService.java
@@ -3,7 +3,6 @@ package com.pts.api.category.application.service;
 import com.pts.api.category.application.port.dto.response.ReadSubCategoryResponse;
 import com.pts.api.category.application.port.in.ReadSubCategoryUseCase;
 import com.pts.api.category.application.port.out.SubCategoryRepositoryPort;
-import com.pts.api.category.domain.model.SubCategory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,26 +17,18 @@ public class SubCategoryService implements ReadSubCategoryUseCase {
     @Override
     @Transactional(readOnly = true)
     public ReadSubCategoryResponse getSubCategory(Long categoryId, Long id) {
-        return mapToDto(subCategoryRepositoryPort.findOneById(categoryId, id)
-            .orElseThrow(() -> new RuntimeException("SubCategory not found with id: " + id)));
+        return ReadSubCategoryResponse.fromModel(
+            subCategoryRepositoryPort.findOneById(categoryId, id)
+                .orElseThrow(() -> new RuntimeException("SubCategory not found with id: " + id)));
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<ReadSubCategoryResponse> getSubCategories(Long categoryId) {
         return subCategoryRepositoryPort.findAllByCategoryId(categoryId).stream()
-            .map(this::mapToDto)
+            .map(ReadSubCategoryResponse::fromModel)
             .toList();
     }
 
-    private ReadSubCategoryResponse mapToDto(SubCategory subCategory) {
-        return new ReadSubCategoryResponse(
-            subCategory.getId(),
-            subCategory.getName(),
-            subCategory.getCategoryId(),
-            subCategory.getCreatedAt(),
-            subCategory.getUpdatedAt()
-        );
-    }
 
 }
