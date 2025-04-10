7e49c38181282bff7829fbc8148146eb06fb0fa8
SeongJongHo <whdgh9595@gmail.com>
2025-04-10 20:23:47 +0900

    Feat(like): handler 리스트로 주입받기 위한 로직 변경

--- src/main/java/com/pts/api/like/application/service/ProductLikeCountHandleApplicationService.java
+++ src/main/java/com/pts/api/like/application/service/ProductLikeCountHandleApplicationService.java
@@ -2,18 +2,19 @@ package com.pts.api.like.application.service;
 
 import com.pts.api.global.common.exception.NotFoundException;
 import com.pts.api.lib.internal.shared.event.Event;
-import com.pts.api.lib.internal.shared.event.EventData;
 import com.pts.api.like.application.port.in.ProductLikeCountHandleUseCase;
 import com.pts.api.like.application.service.handler.ProductLikeCountEventHandler;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductLikeCountHandleApplicationService implements ProductLikeCountHandleUseCase {
 
-    private final List<ProductLikeCountEventHandler<EventData>> eventHandlers;
+    private final List<ProductLikeCountEventHandler> eventHandlers;
 
     /**
      * 이벤트를 처리합니다.
@@ -21,7 +22,7 @@ public class ProductLikeCountHandleApplicationService implements ProductLikeCoun
      * @param event 처리할 이벤트
      */
     @Override
-    public void handleEvent(Event<EventData> event) {
+    public void handleEvent(Event event) {
         eventHandlers.stream()
             .filter(handler -> handler.supports(event))
             .findFirst()
